be12d69c8b31ba8f1a5b612c2d6c5a6b729f8595
Billy Hicks <billy_jerome_jr@me.com>
2020-06-01 15:42:31 -0500

    adding homework

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "What is JavaScript?",
+        choices: ["A hipsters favorite beverage", "A programming language", "A programming language built for adding style to HTML", "A new HTML"],
+        answer: "A programming language"
+    },
+    {
+        title: "What built-in method sorts the elements of an array?",
+        choices: ["Sort Method", "Method of Sorts", "Sorta", "A hipsters favorite beverage"],
+        answer: "Sort Method"
+    },
+    {
+        title: "What company developed JavaScript?",
+        choices: ["Google", "Netscape", "A Hipsters favorite beverage", "Adidas"],
+        answer: "Netscape"
+    },
+    {
+        title: "What is 'this' in JavaScript?",
+        choices: ["The object from where it was called", "A Hipsters Favorite beverage", "That", "How you define what you want to change"],
+        answer: "The object from where it was called"
+    },
+    {
+        title: "In Javascript are calculations with fractional numbers guaranteed to be precise?",
+        choices: ["Yes", "No", "Maybe so", "I aint gotta clue"],
+        answer: "No"
+    },
+    {
+        title: "Does JavaScript have ANYTHING to do with a hipsters beverage?",
+        choices: ["No", "Most days", "What is a hipster?", "Every other day"],
+        answer: "No"
+    },
+];
